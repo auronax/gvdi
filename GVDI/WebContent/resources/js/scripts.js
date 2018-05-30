4dbdc6ec55d18a135448571593a3c2473dd0ffa9
auronax <elison.br@gmail.com>
2018-05-30 12:16:46 -0300

    0

--- GVDI/WebContent/resources/js/scripts.js
+++ GVDI/WebContent/resources/js/scripts.js
@@ -1,6 +1,6 @@
 function verificar(xhr, status, args, dlg, tb) {
         if(args.validationFailed) {
-            PF(dlg).jq.effect("shake", {times:5}, 100);
+            PF(dlg).jq.effect("bounce", {times:10}, 100);
         }
         else {
             PF(dlg).hide();
